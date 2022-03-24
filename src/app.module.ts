936613fbe532040bc05253f64e3de89d91916f66
Gabriel Souza <gabriel.souza@publicsoft.com.br>
2022-03-24 16:50:09 -0300

    nest init

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { JogadoresModule } from './jogadores/jogadores.module';
+@Module({
+  imports: [JogadoresModule],
+  exports: [],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}

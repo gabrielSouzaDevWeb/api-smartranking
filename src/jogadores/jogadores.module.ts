f3a828aff74fba7f8a4c361dc63c757ce08f41f5
Gabriel Souza <gabriel.souza@publicsoft.com.br>
2022-08-25 08:57:03 -0300

    new/branch com o projeto do zero

--- src/jogadores/jogadores.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { JogadoresController } from './jogadores/jogadores.controller';
-
-@Module({
-  controllers: [JogadoresController]
-})
-export class JogadoresModule {}
--- src/jogadores/jogadores/jogadores.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('jogadores')
-export class JogadoresController {}
